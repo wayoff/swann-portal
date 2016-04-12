d4dcd6297cae1e1d03e3ce93abd65df4158cde1f
Mark Jestoni Suner <mark_suner@hotmail.com>
2016-04-13 03:24:28 +0800

    added product procedure on show

--- resources/views/pages/category-products/show.blade.php
+++ resources/views/pages/category-products/show.blade.php
@@ -74,9 +74,57 @@
                 <div role="tabpanel" class="tab-pane" id="procedures">
                     @php
                         $procedures = $product->procedures;
+                        $troubleshootings = $product->troubleShooting;
                     @endphp
-                    @if(!$procedures->isEmpty())
+                    @if(!$procedures->isEmpty() || !$troubleshootings->isEmpty())
                         @foreach($procedures as $procedure)
+                            <div class="Product__Show--Procedure-Container">
+                                <span class="Product__Show--Procedure-Title None"> 
+                                    {{ $procedure->name }}
+                                    <button class="pull-right btn btn-xs btn-primary" data-toggle="collapse" data-target="#procedure_{{$procedure->id}}"> Toggle </button>
+                                </span>
+                                <div class="row">
+                                    <div class="col-md-12 padding-10">
+                                        @if($procedure->document_id)
+                                            <a href="{{$procedure->document->getDocument()}}" class="btn btn-primary margin-10"> View Supporting Document</a>
+                                            <br />
+                                        @endif
+                                    </div>
+                                    <div id="procedure_{{ $procedure->id }}" class="collapse">
+                                        @foreach($procedure->steps as $key => $step)
+                                            <div class="col-md-12">
+                                                <div class="Product__Show--Step-Container Question">
+                                                    <div class="row">
+                                                        <div class="col-md-2 padding-remove">
+                                                            <div class="Question__Number--Container">
+                                                                <span class="Question__Number">{{$key + 1}}</span>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-10 padding-remove">
+                                                            <span class="Product__Show--Step-Title"> {{ $step->title }}</span>
+
+                                                            <div class="Product__Show--Step-Content">
+                                                                <p>{{$step->content}}</p>
+                                                                <div>
+                                                                    @if($step->document_id)
+                                                                        <a href="{{$step->document->getDocument()}}" class="btn btn-primary margin-10"> View Supporting Document</a>
+                                                                        <br />
+                                                                    @endif
+                                                                    @if($step->photo_id)
+                                                                        <img src="{{$step->photo->getImage()}}" alt="" class="Product__Show--Step-Content-Image">
+                                                                    @endif
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                        @foreach($troubleshootings as $procedure)
                             <div class="Product__Show--Procedure-Container">
                                 <span class="Product__Show--Procedure-Title None"> 
                                     {{ $procedure->name }}

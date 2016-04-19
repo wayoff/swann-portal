fdd91866dd37bd2160519e836dfd5d8ee5e6f98f
Mark Jestoni Suner <mark_suner@hotmail.com>
2016-04-20 01:54:38 +0800

    fix database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(CategoriesSeed::class);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(CategoriesSeed::class);
+        $this->call(CountriesSeeder::class);
     }
 }

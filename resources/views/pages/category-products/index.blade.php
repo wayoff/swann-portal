045238aafeaa88c3bc426cb664c7dc16595dd462
Mark Jestoni Suner <mark_suner@hotmail.com>
2016-05-04 00:35:48 +0800

    fix container

--- resources/views/pages/category-products/index.blade.php
+++ resources/views/pages/category-products/index.blade.php
@@ -4,30 +4,28 @@
 @section('Title', $category->name)
 
 @section('content')
-    <div class="container">
-        @if($products->isEmpty())
-            <div class="alert alert-warning">
-                <strong>No Product for: <i>{{$category->name}}</i></strong>
-            </div>
-        @else
-            <div class="row">
-              <div class="col-md-12 text-center">
-                  <form class="navbar-form" action="{{route('categories.{id}.products.index', $category->id)}}" role="search">
-                      <div class="input-group Form__Search--Container">
-                          <input type="text" class="form-control" placeholder="Search" name="q" value="{{$q}}">
-                          <div class="input-group-btn Form__Search--Container--button">
-                              <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                          </div>
-                      </div>
-                  </form>
-              </div>
-            </div>
-            @include('partials.tile-products')
-        @endif
-
-        <div class="text-center">
-            <hr />
-            {{$products->render()}}
+  @if($products->isEmpty())
+      <div class="alert alert-warning">
+          <strong>No Product for: <i>{{$category->name}}</i></strong>
+      </div>
+  @else
+      <div class="row">
+        <div class="col-md-12 text-center">
+            <form class="navbar-form" action="{{route('categories.{id}.products.index', $category->id)}}" role="search">
+                <div class="input-group Form__Search--Container">
+                    <input type="text" class="form-control" placeholder="Search" name="q" value="{{$q}}">
+                    <div class="input-group-btn Form__Search--Container--button">
+                        <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                    </div>
+                </div>
+            </form>
         </div>
-    </div>
+      </div>
+      @include('partials.tile-products')
+  @endif
+
+  <div class="text-center">
+      <hr />
+      {{$products->render()}}
+  </div>
 @stop
--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -3,30 +3,28 @@
 @section('Page__Title', 'Product Listing')
 
 @section('content')
-    <div class="container">
-        @if($products->isEmpty())
-            <div class="alert alert-warning">
-                <strong>No Products Yet</strong>
-            </div>
-        @else
-            <div class="row">
-              <div class="col-md-12 text-center">
-                  <form class="navbar-form" action="{{route('products.index')}}" role="search">
-                      <div class="input-group Form__Search--Container">
-                          <input type="text" class="form-control" placeholder="Search" name="q" value="{{$q}}">
-                          <div class="input-group-btn Form__Search--Container--button">
-                              <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                          </div>
-                      </div>
-                  </form>
-              </div>
-            </div>
-            @include('partials.tile-products')
-        @endif
-
-        <div class="text-center">
-            <hr />
-            {{$products->render()}}
+    @if($products->isEmpty())
+        <div class="alert alert-warning">
+            <strong>No Products Yet</strong>
         </div>
+    @else
+        <div class="row">
+          <div class="col-md-12 text-center">
+              <form class="navbar-form" action="{{route('products.index')}}" role="search">
+                  <div class="input-group Form__Search--Container">
+                      <input type="text" class="form-control" placeholder="Search" name="q" value="{{$q}}">
+                      <div class="input-group-btn Form__Search--Container--button">
+                          <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                      </div>
+                  </div>
+              </form>
+          </div>
+        </div>
+        @include('partials.tile-products')
+    @endif
+
+    <div class="text-center">
+        <hr />
+        {{$products->render()}}
     </div>
 @stop

c692af15ffa42ded774b01f38d518af18478c9ff
Mark Jestoni Suner <mark_suner@hotmail.com>
2016-05-04 23:32:06 +0800

    migration

---
+++ database/migrations/2016_04_15_010251_alter_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->integer('faq_category_id')->default(0)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('faq_category_id');
+        });
+    }
+}

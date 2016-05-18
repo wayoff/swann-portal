529699b9bfe819883c59354928799aa378ac9c1e
Mark Jestoni Suner <mark_suner@hotmail.com>
2016-05-19 07:19:29 +0800

    never ending of commodation

--- public/js/commendation.js
+++ public/js/commendation.js
@@ -1,11 +1,25 @@
 ( function() {
     $('.paragraph').fadeOut();
-    var count = 1;
-    $('.paragraph').each( function(index, element) {
-        setTimeout( function() {
-            $('.paragraph').hide();
-            $(element).fadeIn();
-        }.bind(element), count );
-        count += 10000;
-    });
+    var last = $('.paragraph').length - 1;
+
+    function rerender()
+    {
+        var count = 1;
+        $('.paragraph').each( function(index, element) {
+            setTimeout( function() {
+                $('.paragraph').hide();
+                $(element).fadeIn();
+                console.log(index, last);
+                if (index == last) {
+                    console.log('i should render right?');
+                    setTimeout( function() {
+                        rerender();
+                    }, 10000);
+                }
+            }.bind(element, index), count );
+            count += 10000;
+        });
+    }
+
+    rerender();
 }());

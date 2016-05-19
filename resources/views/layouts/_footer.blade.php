5c5319d7d34171a5e06c8b0e7acaeffd9c60f37c
Mark Jestoni Suner <mark_suner@hotmail.com>
2016-05-20 07:20:08 +0800

    fix button text

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -119,7 +119,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-primary">Ok</button>
                 </div>
             </div>
         </div>

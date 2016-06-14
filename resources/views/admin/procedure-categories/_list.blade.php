38dcf5acdc8c82dcc90515d17717669fd4ddf033
Mark Jestoni Suner <mark_suner@hotmail.com>
2016-06-15 06:03:53 +0800

    fix

--- resources/views/admin/procedure-categories/_list.blade.php
+++ resources/views/admin/procedure-categories/_list.blade.php
@@ -4,7 +4,7 @@
             @if(!$procedureCategory->children->isEmpty())
                 <i class="glyphicon glyphicon-minus-sign"></i>
             @endif
-                <form action="{{route('admin.procedure-categories.{id}.destroy', [$procedureCategory->id, $decision->id])}}" method="POST" class="pull-right" style="margin-left: 5px;">
+                <form action="{{route('admin.procedure-categories.destroy', [$procedureCategory->id])}}" method="POST" class="pull-right" style="margin-left: 5px;">
                     {!! csrf_field() !!}
                     {!! method_field('DELETE') !!}
 
@@ -12,7 +12,7 @@
                         <i class="glyphicon glyphicon-trash"></i>
                     </a>
 
-                    <a href="{{route('admin.procedure-categories.{id}.edit', [$procedureCategory->id, $decision->id])}}" title="Edit" data-toggle="tooltip" class="btn-action">
+                    <a href="{{route('admin.procedure-categories.edit', [$procedureCategory->id])}}" title="Edit" data-toggle="tooltip" class="btn-action">
                         <i class="glyphicon glyphicon-pencil"></i>
                     </a>
 

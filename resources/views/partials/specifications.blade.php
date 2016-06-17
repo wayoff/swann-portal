d29614b75cb25974e12cc97bd7df725d3bb59590
Mark Jestoni Suner <mark_suner@hotmail.com>
2016-06-18 00:40:36 +0800

    added partials in specifications

---
+++ resources/views/partials/specifications.blade.php
@@ -0,0 +1,34 @@
+@php 
+    $genSpecifications = $product->specifications()->get();
+@endphp
+@foreach($product->categories as $category)
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">{{ $category->name }}</h3>
+        </div>
+        <div class="panel-body">
+            @if($category->specifications->count() === 0)
+                <div class="alert alert-info">
+                    No specification
+                </div>
+            @else
+                <div class="col-md-12">
+                    <table class="table table-hover">
+                        <tbody>
+                            @foreach($category->specifications as $specification)
+                                <tr>
+                                    <td>{{ $specification->name}}</td>
+                                    @if($specs = $genSpecifications->where('pivot.specification_id', $specification->id)->first())
+                                        <td>{{$specs->pivot->value}}</td>
+                                    @else
+                                        <td> None </td>
+                                    @endif
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @endif
+        </div>
+    </div>
+@endforeach

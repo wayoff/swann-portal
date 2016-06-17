a49746449f022c9698be786f440576225a645008
Mark Jestoni Suner <mark_suner@hotmail.com>
2016-06-18 00:31:49 +0800

    fix troubleshooting

--- resources/views/partials/troubleshooting.blade.php
+++ resources/views/partials/troubleshooting.blade.php
@@ -5,42 +5,43 @@
             <script>
                 console.log(JSON.parse(document.getElementById('check').getAttribute('content')));
             </script>
-            @foreach($procedureCategories->where('parent_id', 0)->sortBy('order') as $key => $parent)
-                @if(!$procedureCategories->where('parent_id', $parent->id)->sortBy('order')->isEmpty())
+            @php
+                $procedureCategoriesParent = $procedureCategories->where('parent_id', 0)->sortBy('order');
+            @endphp
+            @foreach( $procedureCategoriesParent as $key => $parent)
+                @php
+                    $procedureCategoriesChild = $procedureCategories->where('parent_id', $parent->id)->sortBy('order');
+                @endphp
+                @if(!$procedureCategoriesChild->isEmpty() || $procedures->where('procedure_category_id', $parent->id)->first())
                     <div class="panel panel-primary">
                         <div class="panel-heading" style="cursor: pointer;" data-toggle="collapse" data-target="#procedure_{{$parent->id}}">
                             <h3 class="panel-title"> {{ ucwords($parent->name)}} </h3>
                         </div>
                         <div class="panel-body collapse" id="procedure_{{$parent->id}}">
-                            @foreach($procedureCategories->where('parent_id', $parent->id)->sortBy('order') as $key => $procedureCategory)
-                                @if($procedures->where('procedure_category_id', $procedureCategory->id)->first())
-                                <div class="panel panel-default">
-                                    <div class="panel-heading" style="cursor: pointer;" data-toggle="collapse" data-target="#procedure_{{$procedureCategory->id}}">
-                                        <h3 class="panel-title">{{$procedureCategory->name}}</h3>
-                                    </div>
-                                    <div class="panel-body collapse" id="procedure_{{$procedureCategory->id}}">
-                                        @php
-                                            $list = $procedures->where('procedure_category_id', $procedureCategory->id)->all();
-                                        @endphp
-                                            @include('partials._troubleshooting_listing')
+                            @if(!$procedureCategoriesChild->isEmpty())
+                                @foreach($procedureCategories->where('parent_id', $parent->id)->sortBy('order') as $key => $procedureCategory)
+                                    @if($procedures->where('procedure_category_id', $procedureCategory->id)->first())
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading" style="cursor: pointer;" data-toggle="collapse" data-target="#procedure_{{$procedureCategory->id}}">
+                                            <h3 class="panel-title">{{$procedureCategory->name}}</h3>
                                         </div>
-                                    </div>
-                                @endif
-                            @endforeach
-                        </div>
-                    </div>
-                @endif
-    
-                @if($procedures->where('procedure_category_id', $parent->id)->first())
-                    <div class="panel panel-primary">
-                        <div class="panel-heading" style="cursor: pointer;" data-toggle="collapse" data-target="#procedure_{{$parent->id}}">
-                            <h3 class="panel-title">{{$parent->name}}</h3>
-                        </div>
-                        <div class="panel-body collapse" id="procedure_{{$parent->id}}">
-                            @php
-                                $list = $procedures->where('procedure_category_id', $parent->id)->all();
-                            @endphp
-                            @include('partials._troubleshooting_listing')
+                                        <div class="panel-body collapse" id="procedure_{{$procedureCategory->id}}">
+                                            @php
+                                                $list = $procedures->where('procedure_category_id', $procedureCategory->id)->all();
+                                            @endphp
+                                                @include('partials._troubleshooting_listing')
+                                            </div>
+                                        </div>
+                                    @endif
+                                @endforeach
+                            @endif
+
+                            @if($procedures->where('procedure_category_id', $parent->id)->first())
+                                @php
+                                    $list = $procedures->where('procedure_category_id', $parent->id)->all();
+                                @endphp
+                                @include('partials._troubleshooting_listing')
+                            @endif
                         </div>
                     </div>
                 @endif

31971818d6c45a3fa039b533349276e9707b5c7e
Mark Jestoni Suner <mark_suner@hotmail.com>
2016-06-29 06:57:21 +0800

    fix layout

--- resources/views/partials/screenshots.blade.php
+++ resources/views/partials/screenshots.blade.php
@@ -16,22 +16,24 @@
                     <h3 class="panel-title"> {{$screenshotCategory->name}} </h3>
                 </div>
                 <div class="panel-body collapse gallery" id="{{$id}}">
-                    <div class="row">
-                        @foreach($screenshots as $screenshot)
-                            <div class="col-md-3">
-                                <div class="Card">
-                                    <span class="Card__Title">
-                                        {{$screenshot->name}}
-                                    </span>
-                                    <div class="text-center">
-                                        <a href="{{$screenshot->photo->getImage()}}" class="image-link">
-                                            <img src="{{$screenshot->photo->getImage()}}" style="width: 100%">
-                                        </a>
+                    @foreach($screenshots->chunk(4) as $chunks)
+                        <div class="row">
+                            @foreach($chunks as $screenshot)
+                                <div class="col-md-3">
+                                    <div class="Card">
+                                        <span class="Card__Title">
+                                            {{$screenshot->name}}
+                                        </span>
+                                        <div class="text-center">
+                                            <a href="{{$screenshot->photo->getImage()}}" class="image-link">
+                                                <img src="{{$screenshot->photo->getImage()}}" style="width: 100%">
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endforeach
-                    </div>
+                            @endforeach
+                        </div>
+                    @endforeach
                 </div>
             </div>
         @endif
